996c0f55f3eea9644e34e28aaf023048115ae8cb
rbeylerian <rbeylerian@atreal.fr>
2011-11-23 08:22:14 +0100

    Update README.txt to be reST compliant. Update ZCML to avoid deprecation warning under Plone 4.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.0.0'
+version = '3.1.0'
 
 long_description = (
     read('README.txt')
