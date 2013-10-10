f65d0d290f8b8259a3d34ef1ebaedeb9a0bb33aa
Florent Michon <florent.michon@gmail.com>
2013-10-10 19:45:39 -0300

    Change the structure of the package to have a buildout and an src folder

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Installer for the massloader.atreal.massloader package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
 import os
-from setuptools import setup, find_packages
 
 
 def read(*rnames):
@@ -7,39 +12,57 @@ def read(*rnames):
 
 version = '3.2.0.dev0'
 
-long_description = (
-    read('README.rst')
-    + '\n' +
-    read('docs', 'HISTORY.txt')
-    )
+long_description = \
+    read('README.rst') + \
+    read('CHANGES.rst') + \
+    read('docs', 'LICENSE.rst')
 
 setup(name='atreal.massloader',
-      version=version,
-      description="Add an action to import zip files.",
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+    version=version,
+    description="Add an action to import zip files.",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='atReal',
+    author_email='contact@atreal.fr',
+    url='http://pypi.python.org/pypi/atreal.massloader',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['atreal'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Pillow',
+        'Plone',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'mock',
+            'plone.app.testing',
+            'unittest2',
         ],
-      keywords='',
-      author='atReal',
-      author_email='contact@atreal.net',
-      url='https://svn.plone.org/svn/collective/atreal.massloader/trunk',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['atreal'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'pylzma',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+        'develop': [
+            'coverage',
+            'flake8',
+            'jarn.mkrelease',
+            'plone.app.debugtoolbar',
+            'plone.reload',
+            'Products.Clouseau',
+            'Products.PDBDebugMode',
+            'Products.PrintingMailHost',
+            'Sphinx',
+            'zest.releaser',
+            'zptlint',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
