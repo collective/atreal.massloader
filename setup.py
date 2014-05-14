1ef332a690887763fc743b524e1e2797b4da1b99
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2014-05-14 15:22:16 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.1.3dev'
+version = '3.1.4dev'
 
 long_description = (
     read('README.rst')
